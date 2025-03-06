23ecbc6ee4a1dd59e5583b750eb60b2f527c6b4f
ErikRidhoFirm <erikridh1@gmail.com>
2025-03-06 19:02:09 +0700

    menambahkan route

--- minggu02/PWL_ErikRidho/routes/web.php
+++ minggu02/PWL_ErikRidho/routes/web.php
@@ -57,3 +57,60 @@ Route::resource('photos', PhotoController::class)->except([
 ]);
 
 Route::get('/greeting', [WelcomeController::class, 'greeting']);
+
+//================= Route Name ==================
+//Route::get('/user/profile', function () {
+    //
+//})->name('profile');
+
+//Route::get(
+    //'/user/profile',
+    //[UserProfileController::class, 'show']
+//)->name('profile');
+
+//Generating URLs...
+//$url = route('profile');
+
+//Generating Redirects...
+//return redirect()->route('profile');
+
+
+//============= Route Group dan Route Prefixes ==============
+// Route::middleware(['first', 'second'])->group(function () {
+//     Route::get('/', function () {
+//         // Uses first & second middleware
+//     });
+
+//     Route::get('/user/profile', function() {
+//         // Uses first & second middleware
+//     });
+
+//     Route::domain('(account).example.com')->group (function () {
+//         Route::get('user/{id}', function ($account, $id) {
+//             //
+//         });
+//     });
+
+//     Route::middleware('auth')->group (function () {
+//         Route::get('/user', [UserController::class, 'index']);
+//         Route::get('/post', [PostController::class, 'index']);
+//         Route::get('/event', [EventController::class, 'index']);
+//     })
+// });
+
+
+//================== Route Prefixes =======================
+// Route::prefix('admin')->group(function () {
+//     Route::get('/user', [UserController::class, 'index']);
+//     Route::get('/post', [PostController::class, 'index']);
+//     Route::get('/event', [EventController::class, 'index']);
+//     });
+
+
+//================== Redirect Routes =====================
+// Route::redirect('/here', '/there');
+
+
+//================== View Routes =========================
+// Route::view('/welcome', 'welcome');
+// Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
