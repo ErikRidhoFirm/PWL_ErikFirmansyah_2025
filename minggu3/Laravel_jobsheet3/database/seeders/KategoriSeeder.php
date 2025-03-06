80d95142322a397b37ecd3a4c3ca356b8e47b27d
ErikRidhoFirm <erikridh1@gmail.com>
2025-03-07 03:17:06 +0700

    membuat seeder untuk table lain

--- minggu3/Laravel_jobsheet3/database/seeders/KategoriSeeder.php
+++ minggu3/Laravel_jobsheet3/database/seeders/KategoriSeeder.php
@@ -2,16 +2,41 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class KategoriSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        $data = [
+            [
+                'kategori_id' => 1,
+                'kategori_kode' => 'ELEC',
+                'kategori_nama' => 'Elektronik'
+            ],
+            [
+                'kategori_id' => 2,
+                'kategori_kode' => 'DRINK',
+                'kategori_nama' => 'Minuman'
+            ],
+            [
+                'kategori_id' => 3,
+                'kategori_kode' => 'FOOD',
+                'kategori_nama' => 'Makanan'
+            ],
+            [
+                'kategori_id' => 4,
+                'kategori_kode' => 'TOOL',
+                'kategori_nama' => 'Alat'
+            ],
+            [
+                'kategori_id' => 5,
+                'kategori_kode' => 'EXEC',
+                'kategori_nama' => 'Olahraga'
+            ],
+        ];
+
+        DB::table('m_kategori')->insert($data);
     }
 }
