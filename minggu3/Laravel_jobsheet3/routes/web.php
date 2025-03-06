7674401a340eb009e94aa573ebdfa1bd83d26cf2
ErikRidhoFirm <erikridh1@gmail.com>
2025-03-07 04:01:47 +0700

    memodifikasi route

--- minggu3/Laravel_jobsheet3/routes/web.php
+++ minggu3/Laravel_jobsheet3/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Controller;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/level', [LevelController::class, 'index']);
