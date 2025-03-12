679fdf147238017160ab8ff1b4ed71c33cdf5660
ErikRidhoFirm <erikridh1@gmail.com>
2025-03-12 17:09:07 +0700

    membuat script kode praktikkum 2.3 nomer 3

--- minggu4/Laravel_jobsheet4/app/Http/Controllers/UserController.php
+++ minggu4/Laravel_jobsheet4/app/Http/Controllers/UserController.php
@@ -64,8 +64,11 @@ class UserController extends Controller
         // $user = UserModel::where('username', 'manager9')->firstOrFail();
         // return view('user', ['data' => $user]);
 
+        // $user = UserModel::where('level_id', 2)->count();
+        // dd($user); //digunakan untuk memberhentikan eksekusi kode dan menampilkan jumlah pengguna
+        // return view('user', ['data' => $user]);
+
         $user = UserModel::where('level_id', 2)->count();
-        dd($user);
         return view('user', ['data' => $user]);
     }
 }
