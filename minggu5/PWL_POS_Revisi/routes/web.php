dd06ad39bea2805f331e241021636090eb423e16
ErikRidhoFirm <erikridh1@gmail.com>
2025-03-27 21:28:34 +0700

    modifikasi route web.php prak-3 no-3

--- minggu5/PWL_POS_Revisi/routes/web.php
+++ minggu5/PWL_POS_Revisi/routes/web.php
@@ -17,30 +17,42 @@ use Illuminate\Support\Facades\Controller;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/level', [LevelController::class, 'index']);
+// Route::get('/kategori', [KategoriController::class, 'index']);
+// Route::get('/user', [UserController::class, 'index']);
+
+// //====== Jobsheet 4 =======
+// //praktikkum 2.6 no.5
+// Route::get('/user/tambah', [UserController::class, 'tambah']);
+
+// //praktikkum 2.6 no.8
+// Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+
+// //praktikkum 2.6 no. 12
+// Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+
+// //praktikkum 2.6 no. 15
+// Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+
+// //praktikkum 2.6 no. 18
+// Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+
+// // jobsheet 5 revisi 
+// // praktikkum 2 no 4
+// Route::get('/', [WelcomeController::class, 'index']);
+
+// jobsheet 5 praktikkum 3 no 3
+Route::group(['prefix' => 'user'], function() {
+    Route::get('/', [UserController::class, 'index']);          //menampilkan halaman awal user
+    Route::post('/list', [UserController::class, 'list']);      //menampilkan data user dalam bentuk json untuk datatables
+    Route::get('/create', [UserController::class, 'create']);   //menampilkan halaman form tambah user
+    Route::post('/', [UserController::class, 'store']);         //menyimpan data user baru
+    Route::get('/{id}', [UserController::class, 'show']);       //menampilkan detail user
+    Route::get('/{id}/edit', [UserController::class, 'edit']);  //menamilkan halaman form edit user
+    Route::put('/{id}', [UserController::class, 'update']);     //menyimpan perubahan data user
+    Route::delete('/{id}', [UserController::class, 'destroy']); //menghapus data user
 });
-
-Route::get('/level', [LevelController::class, 'index']);
-Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/user', [UserController::class, 'index']);
-
-//====== Jobsheet 4 =======
-//praktikkum 2.6 no.5
-Route::get('/user/tambah', [UserController::class, 'tambah']);
-
-//praktikkum 2.6 no.8
-Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-
-//praktikkum 2.6 no. 12
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-
-//praktikkum 2.6 no. 15
-Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-
-//praktikkum 2.6 no. 18
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
-
-// jobsheet 5 revisi 
-// praktikkum 2 no 4
-Route::get('/', [WelcomeController::class, 'index']);
