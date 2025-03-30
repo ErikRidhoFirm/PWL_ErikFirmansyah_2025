4ff3ec138278c91b70e3c8a8bc855702904608b2
ErikRidhoFirm <erikridh1@gmail.com>
2025-03-30 17:59:16 +0700

    perbaikan kode tabel Supplier

--- minggu6/PWL_POS/resources/views/supplier/create_ajax.blade.php
+++ minggu6/PWL_POS/resources/views/supplier/create_ajax.blade.php
@@ -54,7 +54,7 @@
                                 title: 'Berhasil',
                                 text: response.message
                             });
-                            dataUser.ajax.reload();
+                            dataSupplier.ajax.reload();
                         } else {
                             $('.error-text').text('');
                             $.each(response.msgField, function(prefix, val) {
