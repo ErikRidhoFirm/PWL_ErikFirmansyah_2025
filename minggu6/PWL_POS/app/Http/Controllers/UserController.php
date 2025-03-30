9d5b594ba36e077a3f55a4d143b6af9a7b1c27a4
ErikRidhoFirm <erikridh1@gmail.com>
2025-03-30 15:06:18 +0700

    menambahkan delete_ajax prak-3 no-5

--- minggu6/PWL_POS/app/Http/Controllers/UserController.php
+++ minggu6/PWL_POS/app/Http/Controllers/UserController.php
@@ -566,4 +566,25 @@ public function confirm_ajax(string $id){
 
     return view('user.confirm_ajax', ['user' => $user]);
 }
+
+// prak-3 no-5
+public function delete_ajax(Request $request , $id){
+    //cek apakah request dari ajax
+    if ($request->ajax() || $request->wantsJson()){
+        $user = UserModel::find($id);
+        if ($user) {
+            $user->delete();
+            return response()->json([
+                'status' => true,
+                'message' => 'Data berhasil dihapus'
+            ]);
+        }else{
+            return response()->json([
+                'status' => false,
+                'message' => 'Data tidak ditemukan'
+            ]);
+        }
+    }
+    return redirect('/');
+}
 }
