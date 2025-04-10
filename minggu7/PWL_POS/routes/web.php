09af00d4294e1ed3886e1f87e110ba59476356dd
ErikRidhoFirm <erikridh1@gmail.com>
2025-04-10 09:26:39 +0700

    perbaikan kode prak-2

--- minggu7/PWL_POS/routes/web.php
+++ minggu7/PWL_POS/routes/web.php
@@ -215,12 +215,12 @@ Route::middleware(['auth'])->group(function(){  // artinya semua route didalam g
     // artinya semua route didalam group ini harus punya role ADM (Administrator)
     Route::middleware(['authorize:ADM'])->group(function(){
         Route::get('/level', [LevelController::class, 'index']);        
-        Route::get('/level/list', [LevelController::class, 'list']);        // untuk list json datatables 
+        Route::post('/level/list', [LevelController::class, 'list']);        // untuk list json datatables 
         Route::get('/level/create', [LevelController::class, 'create']);    
-        Route::get('/level', [LevelController::class, 'store']);        
+        Route::post('/level', [LevelController::class, 'store']);        
         Route::get('/level/{id}/edit', [LevelController::class, 'edit']);   // untuk tampilkan form edit  
-        Route::get('/level/{id}', [LevelController::class, 'update']);      // untuk proses update data
-        Route::get('/level{id}', [LevelController::class, 'destroy']);      // untuk proses hapus data
+        Route::put('/level/{id}', [LevelController::class, 'update']);      // untuk proses update data
+        Route::delete('/level/{id}', [LevelController::class, 'destroy']);      // untuk proses hapus data
     });
 
     // Route Supplier
@@ -241,23 +241,6 @@ Route::middleware(['auth'])->group(function(){  // artinya semua route didalam g
     Route::delete('/{id}', [SupplierController::class, 'destroy']); //menghapus data user
 });
 
-// Route level
-Route::group(['prefix' => 'level'], function() {
-    Route::get('/', [LevelController::class, 'index']);          //menampilkan halaman awal user
-    Route::post('/list', [LevelController::class, 'list']);      //menampilkan data user dalam bentuk json untuk datatables
-    Route::get('/create', [LevelController::class, 'create']);   //menampilkan halaman form tambah user
-    Route::post('/', [LevelController::class, 'store']);         //menyimpan data user baru
-    Route::get('/create_ajax', [LevelController::class, 'create_ajax']);     //Menampilkan halaman form tambah user ajax
-    Route::post('/ajax', [LevelController::class, 'store_ajax']);     //Menyimpan datauser baru ajax
-    Route::get('/{id}', [LevelController::class, 'show']);       //menampilkan detail user
-    Route::get('/{id}/edit', [LevelController::class, 'edit']);  //menamilkan halaman form edit user
-    Route::put('/{id}', [LevelController::class, 'update']);     //menyimpan perubahan data user
-    Route::get('/{id}/edit_ajax', [LevelController::class, 'edit_ajax']);     //menampilkan halaman form edit user ajax
-    Route::put('/{id}/update_ajax', [LevelController::class, 'update_ajax']);     //menyimpan perubahan data user ajax
-    Route::get('/{id}/delete_ajax', [LevelController::class, 'confirm_ajax']);     //untuk tampilkan form confirm delete user ajax
-    Route::delete('/{id}/delete_ajax', [LevelController::class, 'delete_ajax']);     //untuk hapus data user ajax
-    Route::delete('/{id}', [LevelController::class, 'destroy']); //menghapus data user
-});
 
 // Route Kategori
 Route::group(['prefix' => 'kategori'], function() {
