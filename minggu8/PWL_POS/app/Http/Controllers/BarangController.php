991ed4f524669dc5153a0bdd7ede1b666d718ff4
ErikRidhoFirm <erikridho1@gmail.com>
2025-04-21 23:27:06 +0700

    menambahkan fungsi export_excel prak-2 no-4

--- minggu8/PWL_POS/app/Http/Controllers/BarangController.php
+++ minggu8/PWL_POS/app/Http/Controllers/BarangController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use Yajra\DataTables\Facades\DataTables;
 use Illuminate\Support\Facades\Validator;
 use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
 
 class BarangController extends Controller
 {
@@ -399,5 +400,15 @@ class BarangController extends Controller
             }
         }
         return redirect('/');
+    }
+
+    // ===== Jobsheet 8 prak-2 no-4 =====
+    public function export_excel()
+    {
+        // ambil data barang yang akan di export
+        $barang = BarangModel::select('kategori_id', 'barang_kode', 'barang_nama', 'harga_beli', 'harga_jual')
+            ->orderBy('kategori_id')
+            ->with('kategori')
+            ->get();
     }
 }
