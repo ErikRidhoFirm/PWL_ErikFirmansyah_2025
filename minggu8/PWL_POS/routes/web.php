fe74acf29c4335d279b5f21c9690d8c394eaba7b
ErikRidhoFirm <erikridho1@gmail.com>
2025-04-22 16:21:49 +0700

    tambah kode pada route/web.php prak-3 no-3

--- minggu8/PWL_POS/routes/web.php
+++ minggu8/PWL_POS/routes/web.php
@@ -343,6 +343,8 @@ Route::get('/barang/import', [BarangController::class, 'import']); // ajax form 
 Route::post('/barang/import_ajax', [BarangController::class, 'import_ajax']); // ajax import excel
 // ===== Jobsheet 8 prak-2 no-2 =====
 Route::get('/barang/export_excel', [BarangController::class, 'export_excel']); // export excel
+// ===== Jobsheet 8 prak-2 no-2 =====
+Route::get('/barang/export_pdf', [BarangController::class, 'export_pdf']); // export pdf
 
 // route level 
 Route::get('/level', [LevelController::class, 'index']);
