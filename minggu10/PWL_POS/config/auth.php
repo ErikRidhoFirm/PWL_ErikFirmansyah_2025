0e61a67b47d128de9d4c8e93638c4766bae5d6f4
ErikRidhoFirm <erikridho1@gmail.com>
2025-05-02 22:58:45 +0700

    konfigurasi guard API prak-1 no-6

--- minggu10/PWL_POS/config/auth.php
+++ minggu10/PWL_POS/config/auth.php
@@ -40,7 +40,12 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        // ===== Jobsheet 10 prak-1 no-26 =====
+        'api' => [
+            'driver' => 'jwt',
+            'provider' => 'users',
     ],
+],
 
     /*
     |--------------------------------------------------------------------------
